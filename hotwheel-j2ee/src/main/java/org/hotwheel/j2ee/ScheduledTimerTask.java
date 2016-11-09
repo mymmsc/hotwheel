279516b2827a08af32cbb86945fa479905d781f7
wangfeng <wangfengxy@sina.cn>
2016-11-10 07:40:07 +0800

    add task

---
+++ hotwheel-j2ee/src/main/java/org/hotwheel/j2ee/ScheduledTimerTask.java
@@ -0,0 +1,89 @@
+package org.hotwheel.j2ee;
+
+import org.hotwheel.core.BaseContext;
+import org.mymmsc.api.assembly.Api;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定时器
+ *
+ * Created by wangfeng on 16/7/30.
+ */
+public abstract class ScheduledTimerTask extends BaseContext implements ServletContextListener{
+
+    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    /**< 任务 是否正在运行 */
+    protected volatile boolean isRunning = false;
+
+    /**< 任务名 */
+    protected String taskName = null;
+    /**< 任务开始时间 */
+    protected String taskStartTime = null;
+    /**< 任务停止时间 */
+    protected String taskEndTime = null;
+
+
+    //protected abstract String getFixedTime();
+    protected abstract AbstractTask getTask();
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        long msNow = System.currentTimeMillis();
+        long initDelay  = getTimeMillis(taskStartTime) - msNow;
+        initDelay = initDelay > 0 ? initDelay : MillisecondsOfDay + initDelay;
+        AbstractTask task = getTask();
+
+        executor.scheduleAtFixedRate(
+                task,
+                initDelay,
+                MillisecondsOfDay,
+                TimeUnit.MILLISECONDS);
+        isRunning = true;
+        logger.info("Task[{}] will be executed at [{}]", taskName, Api.toString(new Date(msNow + initDelay), TimeFormat));
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        try {
+            if(executor != null) {
+                executor.shutdown();
+            }
+        } catch (Exception e) {
+            error(e);
+        }
+
+        isRunning = false;
+    }
+
+    /**
+     * 获取指定时间对应的毫秒数
+     * @param time "HH:mm:ss"
+     * @return
+     */
+    private long getTimeMillis(String time) {
+        long lRet = 0;
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+            DateFormat dayFormat = new SimpleDateFormat("yy-MM-dd");
+            Date curDate = dateFormat.parse(dayFormat.format(new Date()) + " " + time);
+            lRet = curDate.getTime();
+        } catch (ParseException e) {
+            logger.error("", e);
+        }
+        return lRet;
+    }
+
+    protected void error(Exception e) {
+        String prefix = String.format("Task[%s]", taskName);
+        logger.error(prefix + "关闭定时器失败", e);
+    }
+}
