614fbd5ab688628c748a4bb80a4c88c8041ea23d
王锋 <wangfeng@jiedaibao.com>
2016-11-10 07:40:07 +0800

    add task

---
+++ hotwheel-j2ee/src/main/java/org/hotwheel/j2ee/AbstractTask.java
@@ -0,0 +1,65 @@
+package org.hotwheel.j2ee;
+
+import org.mymmsc.api.assembly.Api;
+import org.mymmsc.api.assembly.ResourceApi;
+
+import java.util.Date;
+import java.util.ResourceBundle;
+
+/**
+ * 抽象任务类
+ *
+ * Created by wangfeng on 16/7/31.
+ */
+public abstract class AbstractTask extends ScheduledTimerTask implements TaskRunnable{
+    private static ResourceBundle rb = null;
+
+    static {
+        rb = ResourceApi.getBundle("timer");
+    }
+
+    @Override
+    public void run() {
+        if(isTimerTask()) {
+            logger.info("{} start.", taskName);
+            doTask();
+            isRunning = false;
+            logger.info("{} end", taskName);
+        } else {
+            logger.info("本机[{}] 不执行定时任务{}", Api.getLocalIp(), taskName);
+        }
+    }
+
+    @Override
+    protected AbstractTask getTask() {
+        return this;
+    }
+
+    protected static String getTimerValue(String key){
+        String sRet = null;
+        try {
+            sRet = rb.getString(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sRet;
+    }
+
+    /**
+     * 是否定时器运行周期内
+     * @return
+     */
+    protected boolean isTimerCycle() {
+        boolean bRet = false;
+        Date date = new Date();
+        String now = Api.toString(date, "HH:mm:ss");
+        if(isRunning &&  now.compareTo(taskStartTime) >= 0 && now.compareTo(taskEndTime) < 0) {
+            bRet = true;
+        } else {
+            bRet = false;
+        }
+
+        return bRet;
+    }
+}
---
+++ hotwheel-j2ee/src/main/java/org/hotwheel/j2ee/TaskBatchThread.java
@@ -0,0 +1,28 @@
+package org.hotwheel.j2ee;
+
+import org.mymmsc.api.assembly.Api;
+
+import java.io.File;
+
+
+/**
+ * 催收业务数据批量
+ *
+ * Created by wangfeng on 16/8/21.
+ */
+public class TaskBatchThread extends ServiceRunnable {
+
+    private File productFile = null;
+    private File orderFile = null;
+    private File repayFile = null;
+
+
+
+    @Override
+    protected void service() {
+        while (isRunning()) {
+            Api.sleep(100);
+        }
+    }
+
+}
