d9d644dd281856b317804f83810c0c892f213459
wangfeng <wangfengxy@sina.cn>
2016-11-25 08:28:04 +0800

    调整部分类名，避免和工作空间类名冲突

--- hotwheel-json/src/test/java/test/json/InnerResult.java
+++ hotwheel-json/src/test/java/test/json/InnerResult.java
@@ -5,9 +5,9 @@ import java.math.BigDecimal;
 /**
  * Created by wangfeng on 2016/11/21.
  */
-public class InnerApiResult<T> {
+public class InnerResult<T> {
     /**< 接口状态 */
-    public InnerApiError error;
+    public InnerError error;
     /**< 数据区 */
     public T data;
     public BigDecimal a1;
