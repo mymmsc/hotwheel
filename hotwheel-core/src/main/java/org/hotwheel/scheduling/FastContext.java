56f220edc7ecd62e019298cb76de25f517bbbafb
wangfeng <wangfengxy@sina.cn>
2017-01-21 09:36:51 +0800

    扩展线程池方法

--- hotwheel-core/src/main/java/org/hotwheel/scheduling/FastContext.java
+++ hotwheel-core/src/main/java/org/hotwheel/scheduling/FastContext.java
@@ -3,7 +3,7 @@ package org.hotwheel.scheduling;
 /**
  * Created by wangfeng on 2017/1/20.
  */
-public interface FastContext<T extends PartitionContext> {
+public interface FastContext<T> {
 
     /**
      * 合并结果集
