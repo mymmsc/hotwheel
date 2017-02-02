23f36a0601b3624a1b3644638f508ea947ff2801
wangfeng <wangfengxy@sina.cn>
2017-02-02 17:47:24 +0800

    upgrade: 3.1.1

--- hotwheel-core/src/main/java/org/hotwheel/scheduling/PartitionContext.java
+++ hotwheel-core/src/main/java/org/hotwheel/scheduling/PartitionContext.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * 批量上下文
  * Created by wangfeng on 2016/10/26.
+ * @since 3.1.1
  */
 public abstract class PartitionContext<T, V> implements FastContext<V>{
     /** 任务名称 */
