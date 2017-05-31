3d38f02323f716850f74ebc0ced7f924b8f90746
wangfeng <wangfengxy@sina.cn>
2017-05-31 14:39:54 +0800

    修复一处json反射的错误

--- hotwheel-json/src/test/java/test/json/bean/BigDataResult.java
+++ hotwheel-json/src/test/java/test/json/bean/BigDataResult.java
@@ -4,7 +4,7 @@ package test.json.bean;
  * Created by wangfeng on 2016/12/5.
  */
 public class BigDataResult {
-    //BigDataHeader responseHeader;
+    BigDataHeader responseHeader;
     BidDataResponse response;
 
 }
